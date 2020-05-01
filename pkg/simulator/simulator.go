a591b56d9f5017a0e15ee663095bbdf1aa8da58b
emanuele <emanuele.motto@gmail.com>
2020-05-01 18:16:11 +0200

    fixed multiple node bugs

--- pkg/simulator/simulator.go
+++ pkg/simulator/simulator.go
@@ -7,30 +7,40 @@ import (
 	"strings"
 )
 
-const nNodes = 3
-const maxJobs = 1000
-const nPartsOfJob = 9
+const nPartsOfJob = 6
 
 var nodes []Node
-var servedJobs = 0
-var nInputSliced = 0
-var systemClock = 0.0
-var jobSplitted = 0
+var servedJobs int
+var nInputSliced int
+var systemClock float64
+var jobSplitted int
 var inputSplits []string //when a job is splitted this array is populated
-
 var taskCompletion map[string]int
 var currentTask string
 var servingNode int
 var nextTime float64
-var lambdas [nNodes]float64
-
-/*
-	timeOfCompletion stores the time needed for a node to complete
-	its map task (position in array = id node)
-*/
-var timeOfCompletion [nNodes]float64
+var lambdas []float64
+var timeOfCompletion []float64
+var lastNodes int
+var nodeSendQ int
+var rateControl bool
+var nNodes int
+var maxJobs int
 
 func initialize() {
+	servedJobs = 0
+	systemClock = 0
+	nodeSendQ = 0
+	servedJobs = 0
+	servedJobs = 0
+	nInputSliced = 0
+	systemClock = 0
+	jobSplitted = 0
+	taskCompletion = make(map[string]int)
+	arrivalTimes = make(map[string]float64)
+	avgJoinLen = make([]float64, nNodes)
+	lambdas = make([]float64, nNodes)
+	timeOfCompletion = make([]float64, nNodes)
 
 	//initialize nodes
 	for i := 0; i < nNodes; i++ {
@@ -53,65 +63,38 @@ func initialize() {
 
 	//generates the times in which the nodes end the computation of the map tasks
 	for i := 0; i < nNodes; i++ {
-		lambdas[i] = float64(i+1) * 5
-		timeOfCompletion[i] = rand.ExpFloat64() / lambdas[i]
+		lambdas[i] = rand.Float64()
+		//lambdas[i] = 1
 
 	}
 
-	taskCompletion = make(map[string]int)
-	arrivalTimes = make(map[string]float64)
-	avgJoinLen = make([]float64, nNodes)
-
 }
 
+/*
+Sends all the tasks of a job to queues in order of nodeID, starting from
+the node where it stopped last usage.
+*/
 func sendTasksToQueues() {
-	var nodeID = 0
 	var task string
 
 	for range inputSplits {
 		task, inputSplits = inputSplits[0], inputSplits[1:]
-		if nodeID == len(nodes) {
-			nodeID = 0
+		if nodeSendQ == len(nodes) {
+			nodeSendQ = 0
 		}
-		nodes[nodeID].serviceTasksQ = append(nodes[nodeID].serviceTasksQ, task)
+		if len(nodes[nodeSendQ].serviceTasksQ) == 0 {
+			timeOfCompletion[nodeSendQ] = rand.ExpFloat64() / lambdas[nodeSendQ]
+		}
+		nodes[nodeSendQ].serviceTasksQ = append(nodes[nodeSendQ].serviceTasksQ, task)
 		arrivalTimes[task] = systemClock
-		nodeID++
-		/*
-			for nodeID < len(nodes) {
-				if len(nodes[nodeID].serviceTasksQ) < lenQ {
-					nodes[nodeID].serviceTasksQ = append(nodes[nodeID].serviceTasksQ, task)
-					arrivalTimes[task] = systemClock
-					nodeID++
-					if nodeID >= len(nodes) {
-						nodeID = 0
-					}
-					break
-				} else {
-					for n := range nodes {
-						if len(nodes[n].serviceTasksQ) >= lenQ {
-							nFullQueues++
-						} else {
-							nFullQueues = 0
-							break
-						}
-					}
-				}
-				nodeID++
-				if nodeID >= len(nodes) {
-					nodeID = 0
-				}
-				if nFullQueues == len(nodes) {
-					inputSplits = append([]string{task}, inputSplits...)
-					break
-				}
-			}
-			if nFullQueues == len(nodes) {
-				break
-			}
-		*/
+		nodeSendQ++
 	}
 }
 
+/*
+	Controls is every task of a job is comlpeted, if it is the case
+	removes every task from the join queues
+*/
 func reduce() {
 	/*
 		keeps track of how many parts of this job
@@ -143,12 +126,17 @@ func reduce() {
 	}
 }
 
+//remove an element from an array
 func remove(s []string, i int) []string {
 	s[len(s)-1], s[i] = s[i], s[len(s)-1]
 	return s[:len(s)-1]
 }
 
-func Start() {
+//Start the main cycle of the simulator
+func Start(rc bool, n int, jobs int) {
+	rateControl = rc
+	nNodes = n
+	maxJobs = jobs
 	initialize()
 	sendTasksToQueues()
 
@@ -158,33 +146,25 @@ func Start() {
 
 		nextTime = 0
 		for i := range timeOfCompletion {
-			if nextTime == 0 || timeOfCompletion[i] < nextTime {
+			if (nextTime == 0 || timeOfCompletion[i] < nextTime) && timeOfCompletion[i] != 0 {
 				nextTime = timeOfCompletion[i]
 				servingNode = i
 			}
 		}
 
-		/*
-			fmt.Println("---- ITERATION ----")
-			for i2 := range nodes {
-				fmt.Println(nodes[i2].serviceTasksQ)
-
-			}
-			fmt.Println(fmt.Sprint("node serving:", servingNode))
-
-			for i2 := range nodes {
-				fmt.Println(nodes[i2].joinTasksQ)
-
-			}
-		*/
-
 		currentTask = nodes[servingNode].serviceTasksQ[0]
 		nodes[servingNode].lenJoin = len(nodes[servingNode].joinTasksQ)
 
 		updateCounters()
-
+		/*
+			if servingNode == 0 {
+				fmt.Println(nodes[servingNode].serviceTasksQ)
+				fmt.Println(nodes[servingNode].joinTasksQ)
+			}
+		*/
 		//remove task from the service Q and adds it to the join Q
 		nodes[servingNode].serviceTasksQ = nodes[servingNode].serviceTasksQ[1:]
+
 		nodes[servingNode].joinTasksQ = append(nodes[servingNode].joinTasksQ, currentTask)
 		nodes[servingNode].taskCompleted++
 		nodes[servingNode].nk2++
@@ -215,7 +195,9 @@ func Start() {
 			tasks
 		*/
 		for i := range timeOfCompletion {
-			timeOfCompletion[i] = timeOfCompletion[i] - nextTime
+			if timeOfCompletion[i] != 0 {
+				timeOfCompletion[i] = timeOfCompletion[i] - nextTime
+			}
 		}
 
 		reduce()
@@ -233,31 +215,33 @@ func Start() {
 
 		sendTasksToQueues()
 
+		/*
+			calculates a new timeOfCompletion for the servingNode,
+			it considers :
+				-if the servingQ is empty
+				-if ratecontrol is enabled
+				-if the parameter nk2 is >= 0
+		*/
 		if len(nodes[servingNode].serviceTasksQ) == 0 {
 			timeOfCompletion[servingNode] = 0
 		} else {
 			var rate float64
-			if nodes[servingNode].nk2 >= 0 {
+			if nodes[servingNode].nk2 >= 0 && rateControl {
 				rate = float64(nodes[servingNode].nk2 + 1)
-				//var stime = rand.ExpFloat64() / lambda
-				//var srate = 1 / stime
-
 				var t = (rand.ExpFloat64() / lambdas[servingNode])
-				_ = rate
-				_ = t
-				//timeOfCompletion[servingNode] = t * rate
-				timeOfCompletion[servingNode] = (rand.ExpFloat64() / lambdas[servingNode])
+
+				timeOfCompletion[servingNode] = t * rate
 			} else {
 				timeOfCompletion[servingNode] = (rand.ExpFloat64() / lambdas[servingNode])
 
 			}
 
 		}
-		fmt.Print()
 
 	}
 	computeStatistics()
 	printResults()
+	fmt.Println(lambdas)
 }
 
 func printResults() {
