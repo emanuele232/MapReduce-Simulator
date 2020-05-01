78fd7be736403bfc59519ad257494f64729d7bc3
emanuele <emanuele.motto@gmail.com>
2020-05-01 18:28:07 +0200

    added help flags

--- cmd/MRSimulator/main.go
+++ cmd/MRSimulator/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 
 	"gitlab.com/emanuele232/mapreduce-simulator/pkg/simulator"
 	//"math/rand"
@@ -11,8 +12,15 @@ func main() {
 	rateControl := flag.Bool("rateControl", true, "is the flag control enabled?")
 	nNodes := flag.Int("nNodes", 5, "the number of nodes")
 	maxJobs := flag.Int("maxJobs", 50, "number of job to be completed to terminate the simulation")
+	h := flag.Bool("h", false, "display help")
+	help := flag.Bool("help", false, "display help")
 	flag.Parse()
 
-	simulator.Start(*rateControl, *nNodes, *maxJobs)
+	if *h || *help {
+		fmt.Println("display menu")
+	} else {
+		simulator.Start(*rateControl, *nNodes, *maxJobs)
+
+	}
 
 }
